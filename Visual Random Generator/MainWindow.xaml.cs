dab2ba8d4a3d62f9d28fa8a82ba9fc9a01855656
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2016-12-18 00:19:25 +0200

    With zoom animation

--- Visual Random Generator/MainWindow.xaml.cs
+++ Visual Random Generator/MainWindow.xaml.cs
@@ -1,28 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 namespace Visual_Random_Generator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private System.Windows.Threading.DispatcherTimer ZoomTimer { get; } = new System.Windows.Threading.DispatcherTimer();
+        private bool IsZoomed { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DrawLines();
+            //ZoomAnimation(1, 1.1);
+
+            ZoomTimer.Tick += TimerMethod;
+            ZoomTimer.Interval = TimeSpan.FromSeconds(1);
+            ZoomTimer.Start();
         }
+
+        #region Canvas
+        private void TimerMethod(object sender, EventArgs e)
+        {
+            const double from = 1;
+            const double to = 1.1;
+            ZoomAnimation(IsZoomed ? to : from, IsZoomed ? from : to);
+            IsZoomed = !IsZoomed;
+        }
+
+        private void DrawLines()
+        {
+            const int thikness = 4;
+            var line = new Line
+            {
+                X1 = 15,
+                Y1 = 15,
+                X2 = 50,
+                Y2 = 15,
+                StrokeThickness = thikness,
+                Stroke = Brushes.Black
+            };
+
+            ClickCanvas.Children.Add(line);
+
+            const int starter = 15;
+            const int length = 50;
+            var bootomX = ClickCanvas.Width;
+            var bootomY = ClickCanvas.Height;
+
+            //  _
+            // |
+            AddLine(starter - thikness, starter, starter + length, starter);
+            AddLine(starter, starter, starter, starter + length);
+
+            // _|
+            AddLine(bootomX - starter, bootomY - starter, bootomX - starter, bootomY - starter - length);
+            AddLine(bootomX - starter + thikness, bootomY - starter, bootomX - starter - length, bootomY - starter);
+
+            // |_
+            AddLine(starter, bootomY - starter, starter, bootomY - starter - length);
+            AddLine(starter - thikness, bootomY - starter, starter + length, bootomY - starter);
+
+            // _
+            //  |
+            AddLine(bootomX - starter + thikness, starter, bootomX - starter - length, starter);
+            AddLine(bootomX - starter, starter, bootomX - starter, starter + length);
+        }
+
+        private void AddLine(double x1, double y1, double x2, double y2)
+        {
+            var line = new Line
+            {
+                X1 = x1,
+                Y1 = y1,
+                X2 = x2,
+                Y2 = y2,
+                StrokeThickness = 8,
+                Stroke = Brushes.Black
+            };
+
+            ClickCanvas.Children.Add(line);
+        }
+        #endregion
+
+        private void ZoomAnimation(double from, double to)
+        {
+            ScaleTransform trans = new ScaleTransform();
+            ClickCanvas.RenderTransform = trans;
+
+            DoubleAnimation anim = new DoubleAnimation(from, to, TimeSpan.FromSeconds(1));
+            trans.BeginAnimation(ScaleTransform.ScaleXProperty, anim);
+            trans.BeginAnimation(ScaleTransform.ScaleYProperty, anim);
+
+        }
+
     }
 }
